fe15415fce1d545d88ae1f9d0491ffd9f0425ecd
Sathish M <sathishms0305@gmail.com>
2024-01-12 21:54:47 +0530

    task card deign completed

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let inputbox = document.getElementById('textbox')
 let savebtn = document.getElementById('savebtn')
 let taskcard = document.getElementById('taskcard')
 
+let taskbox = document.getElementById('taskbox')
+
 let taskList = []
 
 let saveaction = ()=>{
     let newTask = inputbox.value
     console.log(newTask)
     taskList.push(newTask)
+    console.log(taskList)
 }
-
-console.log(taskList)
 savebtn.addEventListener('click', saveaction)
